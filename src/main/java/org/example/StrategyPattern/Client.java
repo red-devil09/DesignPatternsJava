8c241b87447cc77975c81f5465e4e377698717ef
Nikhil Yadav <nikhil.yadav@lowes.com>
2023-05-13 21:10:37 +0530

    Strategy Pattern

--- src/main/java/org/example/StrategyPattern/Client.java
+++ src/main/java/org/example/StrategyPattern/Client.java
@@ -8,6 +8,12 @@ public class Client {
         //see setter injection
         //ab konsa object set krna hai, iske liye we can use factory -> lekin strategy ka kaam itna hi tha
         //setter ka zrur use kro as issi se hi runtime mei we can change things
+        /**
+         * Context -> payment service
+         * strategy -  interface
+         * UpiPayment -> concrete strategy
+         * client -> to ye hai hi
+         */
         paymentService.setPaymentStrategy(paymentStrategy);
         paymentService.processOrderV3();
     }
